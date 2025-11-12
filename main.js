2c1e9c55149f24f5b70f00aaadf4ea185fd21a54
kairy11 <kairy.le0629@gmail.com>
2025-11-11 20:01:52 -0600

    Added mutate method and the compare DNA method. To the factory function.

--- main.js
+++ main.js
@@ -17,13 +17,43 @@ const mockUpStrand = () => {
 const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum,
-    dna
+    dna,
+    mutate() {
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
+      const currentBase = this.dna[randomIndex];
+      let newBase = returnRandBase();
+      while (newBase === currentBase) {
+        newBase = returnRandBase;
+      };
+      this.dna[randomIndex] = newBase;
+      return this.dna;
+    },
+    compareDNA(anotherOrganism) {
+      let counter = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === anotherOrganism.dna[i]) {
+          counter++;
+        }
+      }
+      const percent = ((counter / this.dna.length) * 100).toFixed(2);
+      console.log(`Specimen ${this.specimenNum} and specimen ${anotherOrganism.specimenNum} have ${percent}% DNA in common.`);
+    }
   };
 };
 
-console.log(pAequorFactory(1, ['A', 'T', 'C', 'G', 'A', 'C', 'T', 'A', 'G', 'T', 'C', 'A', 'G', 'C', 'T']));
+/*Test the mutate method
+const testSubject = pAequorFactory(1, mockUpStrand());
+console.log('Before mutation:', testSubject.dna);
+testSubject.mutate();
+console.log('After mutation:', testSubject.dna);
+*/
 
-
-
+/*Test the compare DNA method
+const organism1 = pAequorFactory(1, mockUpStrand());
+const organism2 = pAequorFactory(2, mockUpStrand());
+console.log('Organism 1 DNA:', organism1.dna);
+console.log('Organism 2 DNA:', organism2.dna);
+organism1.compareDNA(organism2);
+*/
 
 
